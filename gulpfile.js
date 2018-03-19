1e01dd666f6c571dab5434283b0c823cef7b8b1e
Benjamin de Bos <info@its-vision.nl>
2018-03-19 12:32:44 +0100

    Removed Grunt introduced Gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
